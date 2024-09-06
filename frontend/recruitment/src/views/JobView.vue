8bbac6a6a9e6d055404afcd9c76c93e8c73f7663
Arshat Aitkozha <arshikoffice@gmail.com>
2024-09-07 01:16:15 +0600

    feat: add job view and toast

--- frontend/recruitment/src/views/JobView.vue
+++ frontend/recruitment/src/views/JobView.vue
@@ -42,7 +42,7 @@
                 class="flex justify-center mb-4 text-gray-500 align-middle md:justify-start"
               >
                 <i
-                  class="mr-2 text-lg text-orange-700 fa-solid fa-location-dot"
+                  class="mr-2 text-xl text-orange-700 pi pi-map-marker"
                 ></i>
                 <p class="text-orange-700">{{state.job.location}}</p>
               </div>
