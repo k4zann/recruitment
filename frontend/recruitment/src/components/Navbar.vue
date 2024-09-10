3a2c7c3b42d2419b9876ba65c616fa9aa89dbfb4
Arshat Aitkozha <arshikoffice@gmail.com>
2024-09-10 23:34:29 +0600

    fix: the color of the active bar in nav bar

--- frontend/recruitment/src/components/Navbar.vue
+++ frontend/recruitment/src/components/Navbar.vue
@@ -27,28 +27,28 @@
           <RouterLink
             to="/"
             :class="[isActiveLink('/') 
-              ? 'bg-blue-900' 
+              ? 'bg-gray-900' 
               : 'hover:bg-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']"
             >Главная</RouterLink
           >
           <RouterLink
             to="/jobs"
             :class="[isActiveLink('/jobs') 
-              ? 'bg-blue-900' 
+              ? 'bg-gray-900' 
               : 'hover:bg-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']"
             >Вакансии</RouterLink
           >
           <RouterLink
             to="/jobs/add"
             :class="[isActiveLink('/jobs/add') 
-              ? 'bg-blue-900' 
+              ? 'bg-gray-900' 
               : 'hover:bg-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']"
             >Добавить вакансию</RouterLink
           >
           <RouterLink
             to="/resume/add"
             :class="[isActiveLink('/resume/add') 
-              ? 'bg-blue-900' 
+              ? 'bg-gray-900' 
               : 'hover:bg-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']"
             >Добавить резюме</RouterLink
           >
