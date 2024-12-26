f68d182589d154b79e9bb1b9969f523854cd953b
Arshat Aitkozha <arshikoffice@gmail.com>
2024-12-27 00:57:36 +0500

    fix: instagram show

--- frontend/recruitment/src/main.js
+++ frontend/recruitment/src/main.js
@@ -5,6 +5,21 @@ import 'vue-toastification/dist/index.css';
 
 import router from './router';
 
+<script async src="https://www.instagram.com/embed.js"></script>
+
+export default function (Vue, { head }) {
+    head.script.push({
+      src: 'https://www.instagram.com/embed.js',
+      async: true
+    })
+  
+    head.meta.push({
+      name: 'keywords',
+      content: 'HTML,CSS,XML,JavaScript'
+    })
+  }
+  
+
 
 import { createApp } from 'vue';
 import App from './App.vue';
