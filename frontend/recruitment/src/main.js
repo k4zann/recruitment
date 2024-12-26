c36e496241d58e4e79f4500e71eda4b44b40844a
Arshat Aitkozha <arshikoffice@gmail.com>
2024-12-27 00:50:56 +0500

    Revert "fix: embed instagram from main.js"
    This reverts commit 4e30597b674bbd4192e1b8a0198814198acd5a44.

--- frontend/recruitment/src/main.js
+++ frontend/recruitment/src/main.js
@@ -9,14 +9,6 @@ import router from './router';
 import { createApp } from 'vue';
 import App from './App.vue';
 
-export default function (Vue, { head }) {
-    head.script.push({  
-        src: 'https://www.instagram.com/embed.js',
-        async: true
-    })
-}
-  
-
 const app = createApp(App);
 
 app.use(router);
