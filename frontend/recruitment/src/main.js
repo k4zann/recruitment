4e30597b674bbd4192e1b8a0198814198acd5a44
Arshat Aitkozha <arshikoffice@gmail.com>
2024-12-26 23:39:19 +0500

    fix: embed instagram from main.js

--- frontend/recruitment/src/main.js
+++ frontend/recruitment/src/main.js
@@ -9,6 +9,14 @@ import router from './router';
 import { createApp } from 'vue';
 import App from './App.vue';
 
+export default function (Vue, { head }) {
+    head.script.push({  
+        src: 'https://www.instagram.com/embed.js',
+        async: true
+    })
+}
+  
+
 const app = createApp(App);
 
 app.use(router);
